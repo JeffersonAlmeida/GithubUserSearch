39c625b41e71910b3749c85b18784b72ec51db06
JeffersonAlmeida <jefferson.almeida@quintoandar.com.br>
2017-01-19 12:42:55 -0200

    done

--- app/src/main/java/com/gus/githubusersearch/MainActivity.java
+++ app/src/main/java/com/gus/githubusersearch/MainActivity.java
@@ -2,16 +2,13 @@ package com.gus.githubusersearch;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import javax.inject.Inject;
-
-import rx.Observable;
 
 public class MainActivity extends AppCompatActivity {
 
     @Inject
-    GusApplication application;
+    GusApplication gusApplication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         GusApplication.getGusComponents().inject(this);
-
-        Observable.just(1).subscribe( t -> {
-            Log.e("T", "onCreate: " + t);
-        });
 
     }
 }
